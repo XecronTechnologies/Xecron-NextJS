5274fedbc5d3d07e843de7169f6fc3933037cedd
xecrontech <xecrontech@gmail.com>
2025-04-13 21:01:50 +0000

    added Navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata ,Viewport} from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "@/redux/providers"; //https://chat.deepseek.com/a/chat/s/01b4bdf6-f162-494c-ae83-6407334f3a9c
+import Navbar from "@/components/common/Navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,7 +28,7 @@ return(
     <body className={inter.className}>
       <Providers>
         <div className="min-h-screen flex flex-col">
-          <h1>Nav bar Setup</h1> {/* Change with Component */}
+          <Navbar
           <main className="flex-grow">{children}</main>
           <h1>Footer Setup</h1> {/* Change with Component */}
         </div>
