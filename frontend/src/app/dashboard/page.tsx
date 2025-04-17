1e1029c01d00e7d7d3f4547c6d3fda84e939afad
xecrontech <xecrontech@gmail.com>
2025-04-17 19:29:10 +0000

    added test dashboard page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+// "use client"
+
+const Dashboard:React.FC = () => {
+    // const a = window.location.href("https://xmind.ai/SyMHBd6l");
+    return (
+        <div>
+         <iframe src="https://xmind.ai/embed/SyMHBd6l?sheet-id=54f01f9c-75da-42b4-8331-af5761e811de" width="100%" height="720px"  scrolling="no" allow="fullscreen"></iframe>
+        </div>
+    );
+};
+
+export default Dashboard;
