b4016ba2779eb96ef7fe6b6a77b89335d365d430
xecrontech <xecrontech@gmail.com>
2025-04-18 10:04:45 +0000

    Navbar connected with navigationbarSlice

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,46 +1,42 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { useAppSelector } from "@/redux/hooks";
 // import { useSelector } from "react-redux";
-// interface User {
-//     id: string;
-//     email: string;
-//     name?:string;
-// }
-// interface AuthState {
-//     user: User | null;
-// }
 
-// interface RootState {
-//     auth: AuthState;
-// }
-const Navbar = () => {
+interface NavItem {
+    name: string;
+    url: string;
+}
+
+const Navbar: React.FC = () => {
     const pathname = usePathname();
-    // const { user } = useSelector((state:RootState) => state.auth);
 
-    const routes =[
-        {path: "/",name:"Home"},
-        // {path:"/about",name:"About"},
-        {path:"/services",name:"Services"},
-        {path:"/contact",name:"Contact"},
-        {path:"/login",name:"Login"}
-    ];
+    const { navItems } = useAppSelector((state) => state.navigationBar);
 
-    return(
-        <nav className="bg-white shadow-sm">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+
+    return (
+        <nav className="bg-white shadow-sm fixed w-[100%]">
+            <div className=" mx-auto px-4 sm:px-6 lg:px-8">    {/* removed max-w-7xl */}
                 <div className="flex justify-between h-16">
-                    <div className="flex item-center">
-                        {/* <Link href="/" className="text-xl font-bold text-gray-900">
-                        Xecron Technologies
-                        </Link> */}
-                        <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                            {routes.map((route)=>(
-                                <Link key={route.path} href={route.path} className={`${pathname===route.path?"border-indigo-500 text-gray-900":"border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}> 
-                                {route.name}
-                                </Link>
-                            ))}
+                    <div className="flex items-center">
+                        <div className="mr-2">
+                            <Image src={"/logo.png"} width={45} height={45} alt="Xecron Technologies Logo" />
+                        </div>
+                        <Link href="/" className="text-xl  text-gray-900 my-auto font-['Calibri']">
+                            <span className="font-bold">Xecron</span> Technologies
+                        </Link>
+                        <div className="hidden sm:ml-20 sm:flex sm:space-x-8">
+                            {navItems.map((route: NavItem) => {
+                                return (
+                                    <Link key={route.url} href={route.url} className={`${pathname === route.url ? "border-indigo-500 text-gray-900" : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}>
+                                        {route.name}
+                                    </Link>
+                                )
+                            })}
                         </div>
                     </div>
                     {/* <div className="flex items-center"> 
