ab4f815723927a4d659344b6be52fdcb4b9c1817
xecrontech <xecrontech@gmail.com>
2025-04-18 10:04:03 +0000

    navigationbar slice file created

---
+++ frontend/src/redux/slices/navigationbarSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface NavItems {
+    name: string;
+    url: string;
+}
+interface NavigationState {
+    navItems: NavItems[];
+}
+
+const initialState: NavigationState = {
+    navItems: [
+        { name: "Home", url: "/" },
+        { name: "Services", url: "/services" },
+        { name: "Career", url: "/career" },
+        { name: "Contact", url: "/contact" },
+        { name: "Login", url: "/login" },
+    ],    
+};
+
+export const navigationbarSlice = createSlice({
+    name: "navigationBar",
+    initialState: initialState,
+    reducers: {
+        updateNavItems: (state, action: PayloadAction<NavItems[]>) => {
+            state.navItems = action.payload;
+        },
+    },
+});
+
+export const { updateNavItems } = navigationbarSlice.actions;
+export default navigationbarSlice.reducer;
