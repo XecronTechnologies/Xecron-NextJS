45ed022112a0140e792319cc12f4bf16cc842643
xecrontech <xecrontech@gmail.com>
2025-04-19 17:44:53 +0000

    added contact reducer

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"
 import navigationReducer from "./slices/navigationbarSlice";
 import footerReducer from "./slices/footerSlice";
+import contactReducer from "./slices/contactSlice"
 
 export const store = configureStore({
     reducer:{
         auth:authReducer,
         navigationBar: navigationReducer,
         footer:footerReducer,
+        contact:contactReducer,
     }
 })
 
