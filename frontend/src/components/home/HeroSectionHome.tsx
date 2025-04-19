cb8d8e6515632b2cb2879c26ce7e9b8e877bd507
xecrontech <xecrontech@gmail.com>
2025-04-19 17:42:59 +0000

    redirect to dashboard url

--- frontend/src/components/home/HeroSectionHome.tsx
+++ frontend/src/components/home/HeroSectionHome.tsx
@@ -6,21 +6,26 @@ export const HeroSectionHome: React.FC = () => {
     // const [check,setCheck] = useState("Sample")
 
     return (
-        <div className="bg-gray-50 py-50  font-['Calibri'] text-lg" >
+        <div className="bg-gray-50 py-30 font-['Calibri'] text-lg">
             <div className="px-4">
                 <div className="container mx-auto">
-                    <div className="flex flex-col items-center mt-10">
-                        <h1 className="text-3xl sm:text-5xl text- ">
+                    <div className="flex flex-col items-center mt-10 text-center">
+                        <div className="w-full">
+                            <h1 className="text-3xl sm:text-5xl">
                             Welcome To&nbsp;
-                            <span className=" font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-blue-500 ">
+                                <span className=" font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-blue-500">
                                 Xecron Technologies
                             </span>
                         </h1>
+                        
+                        <h3 className="text-lg mt-2">
+                            Innovating the Future with Cutting-Edge Solutions. Explore our advanced tech services today!
+                        </h3>
+                        </div>
                     </div>
 
                     <div className="flex justify-center my-6">
-                        
-                        <GradientButton href="/services" text="Start for Free!"/>
+                        <GradientButton href="/contact" text="Get the Apps!" className="mx-2"/>
                         <a href="#" className="py-2 px-4 mx-2 border border-gray-400 rounded-md text-gray-700 font-semibold hover:bg-gray-100 transition duration-300">Documentation</a>
                     </div>
 
