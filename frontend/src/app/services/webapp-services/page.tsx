e680870e1abdea3f98b3b5bd1f5ba248a07d2c28
xecrontech <xecrontech@gmail.com>
2025-04-21 17:07:28 +0000

    web app page created

---
+++ frontend/src/app/services/webapp-services/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const WebAppServicesPage:React.FC = ()=>{
+    return (
+        <div style={{padding:30}}>
+            <h1>Web Page</h1>
+        </div>
+    )
+}
+
+export default WebAppServicesPage;
