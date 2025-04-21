2ad9fa224b2311d98848706380a10052ebe8a76a
xecrontech <xecrontech@gmail.com>
2025-04-21 17:06:38 +0000

    mobile app page created

---
+++ frontend/src/app/services/mobileapp-services/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MobileAppSerivcePage:React.FC = ()=>{
+    return(
+        <div>
+            <h1>Mobile App Service</h1>
+        </div>
+    )
+}
+export default MobileAppSerivcePage;
