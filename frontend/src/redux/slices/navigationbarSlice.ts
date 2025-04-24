893b411bc44d17c4fb2bed198fc325cc66694e9f
xecrontech <xecrontech@gmail.com>
2025-04-24 06:16:17 +0000

    unknown change

--- frontend/src/redux/slices/navigationbarSlice.ts
+++ frontend/src/redux/slices/navigationbarSlice.ts
@@ -11,12 +11,12 @@ interface NavigationState {
 const initialState: NavigationState = {
     navItems: [
         { name: "Home", url: "/" },
-        {name:"Products",url:"/products"},
+        { name: "Products", url: "/products" },
         { name: "Services", url: "/services" },
         { name: "Career", url: "/career" },
         { name: "Contact", url: "/contact" },
-        { name: "Login", url: "/login" },
-    ],    
+        // { name: "Login", url: "/login" },
+    ],
 };
 
 export const navigationbarSlice = createSlice({
