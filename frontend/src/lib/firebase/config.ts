babc43d0fcc87eb4b1bf13b551b90b8c9392aa03
xecrontech <xecrontech@gmail.com>
2025-04-24 06:14:42 +0000

    added handleSignUp and handleGoogleLogin Functions

--- frontend/src/lib/firebase/config.ts
+++ frontend/src/lib/firebase/config.ts
@@ -1,6 +1,10 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1032242589.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1203933501.
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth,createUserWithEmailAndPassword ,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
+import { Dispatch, SetStateAction } from 'react';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyD3u_ozgZVYkQtOgCy4E88i3ekncRKCHSQ",
@@ -15,4 +19,43 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+
+
+
+// Handle SignUp Method
+
+const handleSignUp = async(email: string, password: string, setError: Dispatch<SetStateAction<string>>)=>{
+  try{
+    await createUserWithEmailAndPassword(auth,email,password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log("Sign-Up:  ",user);
+    setError('');
+    })
+  } catch(err) {
+      if (err instanceof Error) {
+        const errorCode = err.name;
+    const errorMessage = err.message;
+        console.log(errorCode, errorMessage);
+        setError("Sign Up Failed: "+errorMessage);
+      }
+  }
+}
+
+// Handle Google Login
+const handleGoogleLogin = async(setError: Dispatch<SetStateAction<string>>)=>{
+  try{
+    const result = await signInWithPopup(auth,googleProvider)
+    console.log("Google Sign-In:  ",result.user);
+    setError('');
+  }catch(err){
+    console.log(err);
+    setError("Google Sign-In Failed: ");
+  }
+}
+
+export { auth, googleProvider, handleGoogleLogin, handleSignUp }
